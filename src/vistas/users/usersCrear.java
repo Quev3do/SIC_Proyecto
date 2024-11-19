d4ec58602d6f2dfeecb3d400c2a87750c62a03cb
Adan Quevedo <adanque12dst@gmail.com>
2024-11-19 10:02:50 -0600

    si
    sissisi (no)

--- src/vistas/users/usersCrear.java
+++ src/vistas/users/usersCrear.java
@@ -26,21 +26,159 @@ public class usersCrear extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        btnInicio = new javax.swing.JButton();
+        lblDesdeMes = new javax.swing.JLabel();
+        txtnombre = new javax.swing.JTextField();
+        cmbrol = new javax.swing.JComboBox<>();
+        lblDesdeMes1 = new javax.swing.JLabel();
+        lblDesdeMes2 = new javax.swing.JLabel();
+        lblDesdeMes3 = new javax.swing.JLabel();
+        txttelefono = new javax.swing.JTextField();
+        txtemail = new javax.swing.JTextField();
+        txtpassword = new javax.swing.JTextField();
+        lblDesdeMes4 = new javax.swing.JLabel();
+        btnborrar = new javax.swing.JButton();
+        btncrear1 = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        btnInicio.setBackground(new java.awt.Color(181, 229, 29));
+        btnInicio.setFont(new java.awt.Font("Meiryo UI", 1, 18)); // NOI18N
+        btnInicio.setText("Inicio");
+        btnInicio.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnInicioActionPerformed(evt);
+            }
+        });
+
+        lblDesdeMes.setBackground(new java.awt.Color(0, 0, 0));
+        lblDesdeMes.setFont(new java.awt.Font("Meiryo UI", 1, 14)); // NOI18N
+        lblDesdeMes.setText("Nombre completo:");
+
+        lblDesdeMes1.setBackground(new java.awt.Color(0, 0, 0));
+        lblDesdeMes1.setFont(new java.awt.Font("Meiryo UI", 1, 14)); // NOI18N
+        lblDesdeMes1.setText("Email:");
+
+        lblDesdeMes2.setBackground(new java.awt.Color(0, 0, 0));
+        lblDesdeMes2.setFont(new java.awt.Font("Meiryo UI", 1, 14)); // NOI18N
+        lblDesdeMes2.setText("telefono:");
+
+        lblDesdeMes3.setBackground(new java.awt.Color(0, 0, 0));
+        lblDesdeMes3.setFont(new java.awt.Font("Meiryo UI", 1, 14)); // NOI18N
+        lblDesdeMes3.setText("Password:");
+
+        txtpassword.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtpasswordActionPerformed(evt);
+            }
+        });
+
+        lblDesdeMes4.setBackground(new java.awt.Color(0, 0, 0));
+        lblDesdeMes4.setFont(new java.awt.Font("Meiryo UI", 1, 14)); // NOI18N
+        lblDesdeMes4.setText("rol:");
+
+        btnborrar.setBackground(new java.awt.Color(255, 102, 102));
+        btnborrar.setFont(new java.awt.Font("Meiryo UI", 1, 18)); // NOI18N
+        btnborrar.setForeground(new java.awt.Color(255, 255, 255));
+        btnborrar.setText("Borrar todo");
+        btnborrar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnborrarActionPerformed(evt);
+            }
+        });
+
+        btncrear1.setBackground(new java.awt.Color(0, 204, 153));
+        btncrear1.setFont(new java.awt.Font("Meiryo UI", 1, 18)); // NOI18N
+        btncrear1.setForeground(new java.awt.Color(255, 255, 255));
+        btncrear1.setText("Crear usuario");
+        btncrear1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btncrear1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnInicio, javax.swing.GroupLayout.PREFERRED_SIZE, 111, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                        .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                            .addGap(17, 17, 17)
+                            .addComponent(btnborrar, javax.swing.GroupLayout.PREFERRED_SIZE, 187, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(btncrear1, javax.swing.GroupLayout.PREFERRED_SIZE, 187, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                .addComponent(lblDesdeMes1)
+                                .addComponent(lblDesdeMes)
+                                .addComponent(lblDesdeMes2)
+                                .addComponent(lblDesdeMes3)
+                                .addComponent(lblDesdeMes4))
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(txttelefono, javax.swing.GroupLayout.PREFERRED_SIZE, 295, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(txtnombre, javax.swing.GroupLayout.PREFERRED_SIZE, 295, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(txtemail, javax.swing.GroupLayout.PREFERRED_SIZE, 295, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(cmbrol, javax.swing.GroupLayout.PREFERRED_SIZE, 295, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(txtpassword, javax.swing.GroupLayout.PREFERRED_SIZE, 295, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                .addContainerGap(36, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(18, 18, 18)
+                .addComponent(btnInicio, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(21, 21, 21)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblDesdeMes)
+                    .addComponent(txtnombre, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblDesdeMes1)
+                    .addComponent(txtemail, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblDesdeMes3)
+                    .addComponent(txtpassword, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblDesdeMes2)
+                    .addComponent(txttelefono, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(cmbrol, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lblDesdeMes4))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 26, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnborrar, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btncrear1, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(28, 28, 28))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnInicioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnInicioActionPerformed
+        Inicio ini = new Inicio();
+        ini.show();
+        this.dispose();
+    }//GEN-LAST:event_btnInicioActionPerformed
+
+    private void txtpasswordActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtpasswordActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtpasswordActionPerformed
+
+    private void btnborrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnborrarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnborrarActionPerformed
+
+    private void btncrear1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btncrear1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btncrear1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +216,18 @@ public class usersCrear extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnInicio;
+    private javax.swing.JButton btnborrar;
+    private javax.swing.JButton btncrear1;
+    private javax.swing.JComboBox<String> cmbrol;
+    private javax.swing.JLabel lblDesdeMes;
+    private javax.swing.JLabel lblDesdeMes1;
+    private javax.swing.JLabel lblDesdeMes2;
+    private javax.swing.JLabel lblDesdeMes3;
+    private javax.swing.JLabel lblDesdeMes4;
+    private javax.swing.JTextField txtemail;
+    private javax.swing.JTextField txtnombre;
+    private javax.swing.JTextField txtpassword;
+    private javax.swing.JTextField txttelefono;
     // End of variables declaration//GEN-END:variables
 }
