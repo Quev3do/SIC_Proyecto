bcf38500f0aa7904c2185a1dc668433d6d677156
Steven Medrano <43816687+medranosteven@users.noreply.github.com>
2024-11-22 00:02:30 -0600

    Update ClaseConexion.java
    antigua conexion

--- src/conexion/ClaseConexion.java
+++ src/conexion/ClaseConexion.java
@@ -3,13 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package conexion;
-
-//import java.sql.*;
-//import java.util.logging.Level;
-//import java.util.logging.Logger;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 /**
@@ -17,39 +11,27 @@ import java.util.logging.Logger;
  * @author Usuario
  */
 public class ClaseConexion {
-    private final String usuario;
-    private final String pass;
-    private final String url;
+    private String usuario;
+    private String pass;
+    private String url;
     private Connection conexion;
-
-    // Constructor
     public ClaseConexion() {
-        this.usuario = "postgres"; // Reemplaza con tu usuario de la base de datos
-        this.pass = "1234"; // Reemplaza con tu contraseña de la base de datos
-        this.url = "jdbc:postgresql://localhost:5432/db_sistemas_contables"; // Reemplaza con tu URL de la base de datos
+        this.usuario = "postgres";
+        this.pass = "adanque12dst";
+        this.url = "jdbc:postgresql://localhost:5432/SIC_BD_2";
     }
-
-    // Método para iniciar la conexión
-    public Connection iniciarConexion() {
+    public Connection iniciarConexion(){
         try {
-            // Cargar el controlador de PostgreSQL
             Class.forName("org.postgresql.Driver");
-            // Establecer la conexión
             conexion = DriverManager.getConnection(url, usuario, pass);
-            // Verificar si la conexión es válida
             boolean estado = conexion.isValid(50000);
-            System.out.println(estado ? "Conexión exitosa" : "Error en la conexión");
+            System.out.println(estado ? "OK":"ERROR");
             return conexion;
         } catch (ClassNotFoundException ex) {
-            Logger.getLogger(ClaseConexion.class.getName()).log(Level.SEVERE, "No se encontró el driver de PostgreSQL", ex);
+            Logger.getLogger(ClaseConexion.class.getName()).log(Level.SEVERE, null, ex);
         } catch (SQLException ex) {
-            Logger.getLogger(ClaseConexion.class.getName()).log(Level.SEVERE, "Error al establecer la conexión", ex);
-        }
+            Logger.getLogger(ClaseConexion.class.getName()).log(Level.SEVERE, null, ex);
+        }  
         return null;
-    }
-
-    // Método para obtener la conexión
-    public Connection getConexion() {
-        return conexion;
     }
 }
