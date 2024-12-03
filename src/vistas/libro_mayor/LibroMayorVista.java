7a3d517e265c70bbcde0b56377cda54ab6a7973c
Adan Quevedo <adanque12dst@gmail.com>
2024-12-03 06:45:53 -0600

    as
    as

--- src/vistas/libro_mayor/LibroMayorVista.java
+++ src/vistas/libro_mayor/LibroMayorVista.java
@@ -66,16 +66,10 @@ public class LibroMayorVista extends javax.swing.JFrame {
         btnInicio = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         tblLibroMayor = new javax.swing.JTable();
-        btnQuitarFiltro = new javax.swing.JButton();
         btnExportarExcel = new javax.swing.JButton();
-        btnAplicarFiltro = new javax.swing.JButton();
-        lblHastaMes = new javax.swing.JLabel();
-        lblDesdeMes = new javax.swing.JLabel();
         lblBalance = new javax.swing.JLabel();
         lblDebe = new javax.swing.JLabel();
         lblHaber = new javax.swing.JLabel();
-        txtfecha2 = new javax.swing.JFormattedTextField();
-        txtfecha1 = new javax.swing.JFormattedTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Libro Mayor");
@@ -107,16 +101,6 @@ public class LibroMayorVista extends javax.swing.JFrame {
         });
         jScrollPane1.setViewportView(tblLibroMayor);
 
-        btnQuitarFiltro.setBackground(new java.awt.Color(220, 101, 57));
-        btnQuitarFiltro.setFont(new java.awt.Font("Meiryo UI", 1, 14)); // NOI18N
-        btnQuitarFiltro.setForeground(new java.awt.Color(255, 255, 255));
-        btnQuitarFiltro.setText("Remover Filtro de Fecha");
-        btnQuitarFiltro.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnQuitarFiltroActionPerformed(evt);
-            }
-        });
-
         btnExportarExcel.setBackground(new java.awt.Color(181, 229, 29));
         btnExportarExcel.setFont(new java.awt.Font("Meiryo UI", 1, 18)); // NOI18N
         btnExportarExcel.setText("Excel");
@@ -125,24 +109,6 @@ public class LibroMayorVista extends javax.swing.JFrame {
                 btnExportarExcelActionPerformed(evt);
             }
         });
-
-        btnAplicarFiltro.setBackground(new java.awt.Color(37, 150, 190));
-        btnAplicarFiltro.setFont(new java.awt.Font("Meiryo UI", 1, 14)); // NOI18N
-        btnAplicarFiltro.setForeground(new java.awt.Color(255, 255, 255));
-        btnAplicarFiltro.setText("Aplicar Filtro de Fecha");
-        btnAplicarFiltro.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnAplicarFiltroActionPerformed(evt);
-            }
-        });
-
-        lblHastaMes.setBackground(new java.awt.Color(0, 0, 0));
-        lblHastaMes.setFont(new java.awt.Font("Meiryo UI", 1, 14)); // NOI18N
-        lblHastaMes.setText("Hasta");
-
-        lblDesdeMes.setBackground(new java.awt.Color(0, 0, 0));
-        lblDesdeMes.setFont(new java.awt.Font("Meiryo UI", 1, 14)); // NOI18N
-        lblDesdeMes.setText("Desde");
 
         lblBalance.setBackground(new java.awt.Color(0, 0, 0));
         lblBalance.setFont(new java.awt.Font("Meiryo UI", 1, 14)); // NOI18N
@@ -155,10 +121,6 @@ public class LibroMayorVista extends javax.swing.JFrame {
         lblHaber.setBackground(new java.awt.Color(0, 0, 0));
         lblHaber.setFont(new java.awt.Font("Meiryo UI", 1, 14)); // NOI18N
         lblHaber.setText("0.00");
-
-        txtfecha2.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.DateFormatter(new java.text.SimpleDateFormat("yyyy-MM-dd"))));
-
-        txtfecha1.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.DateFormatter(new java.text.SimpleDateFormat("yyyy-MM-dd"))));
 
         javax.swing.GroupLayout Jpanel_LibroMayorLayout = new javax.swing.GroupLayout(Jpanel_LibroMayor);
         Jpanel_LibroMayor.setLayout(Jpanel_LibroMayorLayout);
@@ -178,21 +140,8 @@ public class LibroMayorVista extends javax.swing.JFrame {
                     .addGroup(Jpanel_LibroMayorLayout.createSequentialGroup()
                         .addGap(18, 18, 18)
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 42, Short.MAX_VALUE)
-                .addGroup(Jpanel_LibroMayorLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, Jpanel_LibroMayorLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                        .addComponent(btnQuitarFiltro, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(btnAplicarFiltro, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addGroup(Jpanel_LibroMayorLayout.createSequentialGroup()
-                            .addGap(0, 2, Short.MAX_VALUE)
-                            .addGroup(Jpanel_LibroMayorLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                .addComponent(lblDesdeMes, javax.swing.GroupLayout.Alignment.TRAILING)
-                                .addComponent(lblHastaMes, javax.swing.GroupLayout.Alignment.TRAILING))
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                            .addGroup(Jpanel_LibroMayorLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                .addComponent(txtfecha2, javax.swing.GroupLayout.PREFERRED_SIZE, 179, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addComponent(txtfecha1, javax.swing.GroupLayout.PREFERRED_SIZE, 179, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                    .addComponent(btnExportarExcel, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 238, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 41, Short.MAX_VALUE)
+                .addComponent(btnExportarExcel, javax.swing.GroupLayout.PREFERRED_SIZE, 238, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18))
         );
         Jpanel_LibroMayorLayout.setVerticalGroup(
@@ -213,20 +162,9 @@ public class LibroMayorVista extends javax.swing.JFrame {
                             .addComponent(lblHaber))
                         .addGap(27, 27, 27))
                     .addGroup(Jpanel_LibroMayorLayout.createSequentialGroup()
-                        .addGroup(Jpanel_LibroMayorLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(lblDesdeMes)
-                            .addComponent(txtfecha1, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(23, 23, 23)
-                        .addGroup(Jpanel_LibroMayorLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(lblHastaMes)
-                            .addComponent(txtfecha2, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(148, 148, 148)
-                        .addComponent(btnAplicarFiltro, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addComponent(btnQuitarFiltro, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
+                        .addGap(347, 347, 347)
                         .addComponent(btnExportarExcel, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap(64, Short.MAX_VALUE))))
+                        .addContainerGap(68, Short.MAX_VALUE))))
         );
 
         getContentPane().add(Jpanel_LibroMayor, java.awt.BorderLayout.CENTER);
@@ -239,35 +177,6 @@ public class LibroMayorVista extends javax.swing.JFrame {
         ini.show();
         this.dispose();
     }//GEN-LAST:event_btnInicioActionPerformed
-
-    private void btnAplicarFiltroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAplicarFiltroActionPerformed
-        DefaultTableModel modelo = (DefaultTableModel)this.tblLibroMayor.getModel();
-        modelo.setRowCount(0);
-        
-        String fechaI = txtfecha1.getText();
-        String fechaF = txtfecha2.getText();
-        
-        balance balanc = new balance().getTotalDebeHaberPorRango(fechaI,fechaF);
-        ArrayList<libro_mayor_filtro> datosFiltrados = new libro_mayor_filtro().libroMayorFiltro(fechaI,fechaF);
-        
-        for(libro_mayor_filtro dato: datosFiltrados){
-            modelo.addRow(new Object[]{
-                dato.getIdCuenta(),
-                dato.getNombreCuenta(),
-                dato.getTotalDebe(),
-                dato.getTotalHaber()
-            });
-        }
-        
-        this.tblLibroMayor.setModel(modelo);
-        
-        lblDebe.setText(balanc.getDebeGlobal());
-        lblHaber.setText(balanc.getHaberGlobal());
-    }//GEN-LAST:event_btnAplicarFiltroActionPerformed
-
-    private void btnQuitarFiltroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnQuitarFiltroActionPerformed
-        cargarTabla();
-    }//GEN-LAST:event_btnQuitarFiltroActionPerformed
 
     private void btnExportarExcelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnExportarExcelActionPerformed
         ExportExcel EXXC;
@@ -365,18 +274,12 @@ public class LibroMayorVista extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel Jpanel_LibroMayor;
-    private javax.swing.JButton btnAplicarFiltro;
     private javax.swing.JButton btnExportarExcel;
     private javax.swing.JButton btnInicio;
-    private javax.swing.JButton btnQuitarFiltro;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel lblBalance;
     private javax.swing.JLabel lblDebe;
-    private javax.swing.JLabel lblDesdeMes;
     private javax.swing.JLabel lblHaber;
-    private javax.swing.JLabel lblHastaMes;
     private javax.swing.JTable tblLibroMayor;
-    private javax.swing.JFormattedTextField txtfecha1;
-    private javax.swing.JFormattedTextField txtfecha2;
     // End of variables declaration//GEN-END:variables
 }   
